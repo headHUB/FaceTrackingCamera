41bad40422fd6f7325dcabcfa7132aec22f3060f
headHUB <james@theheadspot.com>
2018-07-08 15:15:52 -0500

    made directories and fixed associations

--- FaceTrackingCamera.cpp
+++ FaceTrackingCamera.cpp
@@ -11,7 +11,7 @@
 #include<opencv2/imgproc/imgproc.hpp>
 #include<opencv2/core/core.hpp>
 #include <iostream>
-#include "Tserial.h"
+#include "Arduino/src/tserial.h"
 
 using namespace std;
 using namespace cv;
